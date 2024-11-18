ab41d5dd2ad5c74b26dbabcb6becfb5ad699ff32
ericoder06 <eric.kossovsky@gmail.com>
2024-11-17 21:37:13 -0500

    removed some stuff

--- TicTacToe/Board.cs
+++ TicTacToe/Board.cs
@@ -40,25 +40,25 @@ public enum Player
             // Checks to see if row win occured
             for (int i = 0; i < 3; i++)
             {
-                if (board[i, 0] != Player.None && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
                 {
                     return board[i, 0];
                 }
 
                 // Checks to see if column win occured
-                if (board[0, i] != Player.None && board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
                 {
                     return board[0, i];
                 }
             }
 
             // Check to see if diagonal win occured
-            if (board[0, 0] != Player.None && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
             {
                 return board[0, 0];
             }
 
-            if (board[0, 2] != Player.None && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
             {
                 return board[0, 2];
             }
